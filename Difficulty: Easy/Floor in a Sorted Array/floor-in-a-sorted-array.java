aec859b372632c23cd12dddf27d26586bc60f4cc
Yashwanth Chennu <149840496+yashwanth1011@users.noreply.github.com>
2025-02-15 23:21:32 -0800

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.HashMap;
+
+
+// } Driver Code Ends
+class Solution {
+
+    static int findFloor(int[] arr, int k) {
+        // write code here
+        int left = 0, right = arr.length - 1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(arr[mid] == k) return mid;
+            if(arr[mid] > k)
+                right = mid - 1;
+            else left = mid + 1;
+        }
+        return right;
+    }
+}
+
+
+//{ Driver Code Starts.
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int k = Integer.parseInt(br.readLine());
+            // Create Solution object and find closest sum
+            Solution ob = new Solution();
+            int ans = ob.findFloor(arr, k);
+
+            System.out.print(ans);
+
+            System.out.println(); // New line after printing the results
+        }
+    }
+}
+
+// } Driver Code Ends

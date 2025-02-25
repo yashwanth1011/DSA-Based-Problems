20c7f394429462f61a6a19d53ad4ca68f917bbed
Yashwanth Chennu <149840496+yashwanth1011@users.noreply.github.com>
2025-02-25 13:33:46 -0800

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.java
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.java
@@ -29,8 +29,6 @@ class Main {
 }
 // } Driver Code Ends
 
-
-// User function Template for Java
 class Pair{
     int vertex, distance;
     Pair(int vertex, int distance){
@@ -38,50 +36,52 @@ class Pair{
         this.distance = distance;
     }
 }
+// User function Template for Java
 class Solution {
 
     public int[] shortestPath(int V, int E, int[][] edges) {
         // Code here
-        
-        List<List<Pair>> graph = new ArrayList<>();
-        for(int i =0; i < V; i++)
+        List<List<int[]>> graph = new ArrayList<>();
+        for(int i = 0; i < V; i++)
             graph.add(new ArrayList<>());
+        
+        
         
         for(int i =0; i < edges.length; i++){
             int u = edges[i][0];
             int v = edges[i][1];
-            int distance = edges[i][2];
-            graph.get(u).add(new Pair(v, distance));
+            int wt = edges[i][2];
+            
+            graph.get(u).add(new int[]{v, wt});
+            
         }
+        
         
         int[] distance = new int[V];
         Arrays.fill(distance, Integer.MAX_VALUE);
         
         distance[0] = 0;
+        Queue<Integer> q = new LinkedList<>();
         
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(0, 0));
+        q.add(0);
         while(!q.isEmpty()){
-            Pair temp = q.remove();
-            int vertex = temp.vertex;
-            int dist = temp.distance;
-            for(Pair neigh: graph.get(vertex)){
-                if(dist + neigh.distance < distance[neigh.vertex]){
-                    distance[neigh.vertex] = dist + neigh.distance;
-                    q.add(new Pair(neigh.vertex, distance[neigh.vertex]));
+            int vertex = q.remove();
+            for(int[] neigh : graph.get(vertex)){
+                int node = neigh[0];
+                int wt = neigh[1];
+                if(distance[vertex] + wt < distance[node]){
+                    q.add(node);
+                    distance[node] = wt + distance[vertex];
                 }
             }
         }
         
-        for(int i =0; i < V; i++){
+        for(int i =0; i < V; i++)
             if(distance[i] == Integer.MAX_VALUE)
                 distance[i] = -1;
                 
-        }
         
         return distance;
-        
-        
         
     }
 }
